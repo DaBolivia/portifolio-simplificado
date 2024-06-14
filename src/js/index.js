57cb2f51f63eb154ca424f95c102f220bb98a4d2
Pedro Thiago <150829194+DaBolivia@users.noreply.github.com>
2024-06-14 13:34:06 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -3,12 +3,12 @@ const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)');
 
 botaoMostrarProjetos.addEventListener('click', () => {
     if (botaoMostrarProjetos.classList.contains('mostrando')) {
-        // Se o botão está no estado de mostrando, então vamos esconder os projetos
+     
         esconderProjetos();
         botaoMostrarProjetos.classList.remove('mostrando');
         botaoMostrarProjetos.textContent = 'Mostrar mais';
     } else {
-        // Caso contrário, mostramos os projetos
+     
         mostrarMaisProjetos();
         botaoMostrarProjetos.classList.add('mostrando');
         botaoMostrarProjetos.textContent = 'Mostrar menos';
